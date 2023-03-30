51ac768c3e2b90a2274e6083201b8c53b1e5e9ff
Aleksandruk <e-tzau@gft.com>
2023-03-30 14:19:56 +0200

    test

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PDFDocument } from 'pdf-lib'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create()
+
+  const page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 30
+  page.drawText('Creating PDFs in JavaScript is awesome!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+  })
+
+  const pdfBytes = await pdfDoc.save()
+
+  return Buffer.from(pdfBytes);
+}
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const pdfBytes = await createPdf();
+
+  res.setHeader("Content-type", "application/pdf")
+  res.status(200).send(pdfBytes);
 }
